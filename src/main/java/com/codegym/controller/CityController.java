6a90b6c5d318f61f86caa708b88caed5e2decdf1
lekhachanh <lekhachanh123@gmail.com>
2019-08-19 16:01:52 +0700

    add commit

--- src/main/java/com/codegym/controller/CityController.java
+++ src/main/java/com/codegym/controller/CityController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -31,6 +32,22 @@ public class CityController {
         Iterable<City> cities = cityService.findAll();
         ModelAndView modelAndView = new ModelAndView("/city/list");
         modelAndView.addObject("cities", cities);
+        return modelAndView;
+    }
+
+    @GetMapping("/create")
+    public ModelAndView showCreateForm(){
+        ModelAndView modelAndView = new ModelAndView("/city/create");
+        modelAndView.addObject("city", new City());
+        return modelAndView;
+    }
+
+    @PostMapping("/save")
+    public ModelAndView saveCity(@ModelAttribute("city") City city){
+        cityService.save(city);
+        ModelAndView modelAndView = new ModelAndView(("/city/create"));
+        modelAndView.addObject("city", new City());
+        modelAndView.addObject("message", "new city created");
         return modelAndView;
     }
 
